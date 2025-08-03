3266b96ff2b663163177619c2959bfdeefd6a440
MadenToken <madentoken@gmail.com>
2025-08-04 00:19:04 +0300

    Update includes.js

--- includes.js
+++ includes.js
@@ -1,11 +1,11 @@
 document.getElementById("navbar").innerHTML = `
 <div class="row surface row-items-hor-center row-gutter-1 p-1">
   <!-- Logo -->
-  <div class="col res-col-2 col-items-hor-center">
+  <div class="res-col-2 col-items-hor-center">
     <div style="color:#f0bb0c" class="fs-xxl"><a href="index.html">MADENIX</a></div>
   </div>
   <!-- Menu -->
-  <div class="col res-col-9 col-items-hor-center fs-xl">
+  <div class="res-col-9 col-items-hor-center fs-xl">
     THIS PAGE IS MADE WITH MadenCSS
   </div>
 </div>
@@ -21,17 +21,17 @@ document.getElementById("footer").innerHTML = `
 `;
 
 document.getElementById("leftMenu").innerHTML = `
-    <h3 class="m-1">HOME</h3>
+    <h3 class="m-1">🏠HOME</h3>
     <ul class="m-1">
       <li><a href="index.html">Get Started</a></li>
       <li><a href="layout.html">Layout</a></li>
     </ul>
-    <h3 class="m-1 mt-3">COMPONENTS</h3>
+    <h3 class="m-1 mt-3">🧩COMPONENTS</h3>
     <ul class="m-1">
       <li><a href="#">Navbars</a></li>
     </ul>
 
-    <h3 class="m-1 mt-3">UTULITIES</h3>
+    <h3 class="m-1 mt-3">🛠️UTULITIES</h3>
     <ul class="m-1">
       <li><a href="#">Font Sizes</a></li>
       <li><a href="#">Margin, Padding</a></li>
